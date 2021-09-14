3486e3844e079b1b393df864dddabcdfe1de62ea
HigorRodrigues <higor.italva@gmail.com>
2021-09-13 22:45:40 -0300

    refactoring(routes): Refatorando as rotas do projeto

--- startUp.ts
+++ startUp.ts
@@ -6,14 +6,13 @@ import "./shared/container";
 import { NewsController } from "./controller/newsController";
 import { GaleriaController } from "./controller/galeriaController";
 import { VideosController } from "./controller/videosController";
+import newsRouter from "./router/newsRouter";
+import videosRouter from "./router/videosRouter";
+import galeriaRouter from "./router/galeriaRouter";
 class StartUp {
 
     public app: Application;
     private _db: Database = new Database();
-
-    private news = container.resolve(NewsController);
-    private videos = container.resolve(VideosController);
-    private galeria = container.resolve(GaleriaController)
     
     constructor(){
         this.app = express();
@@ -24,31 +23,11 @@ class StartUp {
     routes(){
         this.app.route("/").get((req: Request, res: Response) => {
             res.send({ versao: "0.0.2" });
-        });
-
-        this.app.route("/api/v1/news/:page/:quantidade").get((req: Request, res: Response) => {
-            return this.news.get(req, res);
-        });
-
-        this.app.route("/api/v1/news/:id").get((req: Request, res: Response) => {
-            return this.news.getById(req, res);
-        });
-
-        this.app.route("/api/v1/videos/:page/:quantidade").get((req: Request, res: Response) => {
-            return this.videos.get(req, res);
-        });
-
-        this.app.route("/api/v1/videos/:id").get((req: Request, res: Response) => {
-            return this.videos.getById(req, res);
-        });
-
-        this.app.route("/api/v1/galeria/:page/:quantidade").get((req: Request, res: Response) => {
-            return this.galeria.get(req, res);
-        });
-
-        this.app.route("/api/v1/galeria/:id").get((req: Request, res: Response) => {
-            return this.galeria.getById(req, res);
-        });
+        }); 
+        
+        this.app.use("/", newsRouter);
+        this.app.use("/", videosRouter);
+        this.app.use("/", galeriaRouter);
     }
 }
 
